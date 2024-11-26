72153db0b0524b96f694ac8fc40d6757cc6c00f0
JofreCoca <jofrecoca@gmail.com>
2024-11-26 07:49:13 +0100

    finish level 2 exercise 2

---
+++ src/Level_2_Exercise_1/Main.java
@@ -0,0 +1,19 @@
+package Level_2_Exercise_1;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args){
+
+        HashSet<Restaurant> hset = new HashSet<Restaurant>();
+
+        hset.add(new Restaurant("Restaurant1",2));
+        hset.add(new Restaurant("Restaurant2",3));
+        hset.add(new Restaurant("Restaurant2",4));
+        hset.add(new Restaurant("Restaurant4",5));
+
+        hset.add(new Restaurant("Restaurant2",3));
+
+        System.out.println(hset);
+    }
+}
---
+++ src/Level_2_Exercise_1/Restaurant.java
@@ -0,0 +1,38 @@
+package Level_2_Exercise_1;
+
+import java.util.Objects;
+
+public class Restaurant {
+    private String name;
+    private int score;
+
+    public Restaurant(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "\nRestaurant{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return score == that.score && Objects.equals(name, that.name);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+}
